d4f43b144acf2affbcef1d9af5c1227e176ee8de
野元 潤一 <021213@midac-jp.local>
2026-01-30 16:31:26 +0900

    Fix: Add deploy settings for Flex Consumption

--- table_writerNew.py
+++
@@ -1,7 +0,0 @@
-if norm_label == normalize_text("議事"):
-    raw_value = re.sub(
-        r"【出力形式】[\s\S]*?---\s*",  # 先頭〜"---" + 改行までマッチ
-        "",
-        str(raw_value),
-        flags=re.MULTILINE
-    )
